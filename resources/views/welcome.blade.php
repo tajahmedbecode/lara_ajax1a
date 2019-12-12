c89e894592af1cf39953a99c9e546304093edec1
capcom <taj.alsir81@gmail.com>
2019-12-12 14:38:05 +0100

    migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    lara_ajax1a
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/grocery', 'grocery');
+Route::post('/grocery/post', 'GroceryController@store');
+
